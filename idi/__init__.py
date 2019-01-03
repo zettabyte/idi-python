40a242c15eb242aba006836023aa62016d7e1f71
Kendall Gifford <zettabyte@gmail.com>
2019-01-02 22:07:10 -0700

    Fix dynamic package version loading code

--- idi/__init__.py
+++ idi/__init__.py
@@ -1,2 +1,13 @@
 # encoding: utf-8
-name = "idi"
+import os
+
+_version = None
+def version():
+    global _version
+    if _version:
+        return _version
+    filename = os.path.abspath(os.path.join(os.path.dirname(__file__), "VERSION"))
+    with open(filename, "r") as f:
+        _version = tuple(int(x) for x in f.read().strip().split("."))
+    return _version
+
